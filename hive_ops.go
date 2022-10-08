aa443cab54b126d6e4b8892632409b2acc309871
Conor <git@dbuidl.com>
2022-10-08 11:29:49 +0100

    Fix Spacing

--- hive_ops.go
+++ hive_ops.go
@@ -3,117 +3,117 @@ package hivego
 import "encoding/hex"
 
 type hiveOperation interface {
-    serializeOp() ([]byte, error)
-    opName() string
+	serializeOp() ([]byte, error)
+	opName() string
 }
 
 type voteOperation struct {
-    Voter string `json:"voter"`
-    Author string `json:"author"`
-    Permlink string `json:"permlink"`
-    Weight int16 `json:"weight"`
-    opText string
+	Voter    string `json:"voter"`
+	Author   string `json:"author"`
+	Permlink string `json:"permlink"`
+	Weight   int16  `json:"weight"`
+	opText   string
 }
 
 func (o voteOperation) opName() string {
-    return o.opText
+	return o.opText
 }
 
-func (h *HiveRpcNode) VotePost(voter string, author string, permlink string, weight int, wif *string) (string, error){
-    vote := voteOperation{voter, author, permlink, int16(weight), "vote"}
+func (h *HiveRpcNode) VotePost(voter string, author string, permlink string, weight int, wif *string) (string, error) {
+	vote := voteOperation{voter, author, permlink, int16(weight), "vote"}
 
-    return h.broadcast([]hiveOperation{vote}, wif)
+	return h.broadcast([]hiveOperation{vote}, wif)
 }
 
 type customJsonOperation struct {
-    RequiredAuths []string `json:"required_auths"`
-    RequiredPostingAuths []string `json:"required_posting_auths"`
-    Id string `json:"id"`
-    Json string `json:"json"`
-    opText string
+	RequiredAuths        []string `json:"required_auths"`
+	RequiredPostingAuths []string `json:"required_posting_auths"`
+	Id                   string   `json:"id"`
+	Json                 string   `json:"json"`
+	opText               string
 }
 
 func (o customJsonOperation) opName() string {
-    return o.opText
+	return o.opText
 }
 
-func (h *HiveRpcNode) BroadcastJson(reqAuth []string, reqPostAuth []string, id string, cj string, wif *string) (string, error){
-    op := customJsonOperation{reqAuth, reqPostAuth, id, cj, "custom_json"}
-    return h.broadcast([]hiveOperation{op}, wif)
+func (h *HiveRpcNode) BroadcastJson(reqAuth []string, reqPostAuth []string, id string, cj string, wif *string) (string, error) {
+	op := customJsonOperation{reqAuth, reqPostAuth, id, cj, "custom_json"}
+	return h.broadcast([]hiveOperation{op}, wif)
 }
 
 func getHiveChainId() []byte {
-    cid, _ := hex.DecodeString("beeab0de00000000000000000000000000000000000000000000000000000000")
-    return cid
+	cid, _ := hex.DecodeString("beeab0de00000000000000000000000000000000000000000000000000000000")
+	return cid
 }
 
 func getHiveOpId(op string) uint64 {
-    op = op + "_operation"
-    hiveOpsIds := getHiveOpIds()
-    return hiveOpsIds[op]
+	op = op + "_operation"
+	hiveOpsIds := getHiveOpIds()
+	return hiveOpsIds[op]
 }
 
 func getHiveOpIds() map[string]uint64 {
-    hiveOpsIds := make(map[string]uint64)
-    hiveOpsIds["vote_operation"] = 0
-    hiveOpsIds["comment_operation"] = 1
+	hiveOpsIds := make(map[string]uint64)
+	hiveOpsIds["vote_operation"] = 0
+	hiveOpsIds["comment_operation"] = 1
 
-    hiveOpsIds["transfer_operation"] = 2
-    hiveOpsIds["transfer_to_vesting_operation"] = 3
-    hiveOpsIds["withdraw_vesting_operation"] = 4
+	hiveOpsIds["transfer_operation"] = 2
+	hiveOpsIds["transfer_to_vesting_operation"] = 3
+	hiveOpsIds["withdraw_vesting_operation"] = 4
 
-    hiveOpsIds["limit_order_create_operation"] = 5
-    hiveOpsIds["limit_order_cancel_operation"] = 6
+	hiveOpsIds["limit_order_create_operation"] = 5
+	hiveOpsIds["limit_order_cancel_operation"] = 6
 
-    hiveOpsIds["feed_publish_operation"] = 7
-    hiveOpsIds["convert_operation"] = 8
+	hiveOpsIds["feed_publish_operation"] = 7
+	hiveOpsIds["convert_operation"] = 8
 
-    hiveOpsIds["account_create_operation"] = 9
-    hiveOpsIds["account_update_operation"] = 10
+	hiveOpsIds["account_create_operation"] = 9
+	hiveOpsIds["account_update_operation"] = 10
 
-    hiveOpsIds["witness_update_operation"] = 11
-    hiveOpsIds["account_witness_vote_operation"] = 12
-    hiveOpsIds["account_witness_proxy_operation"] = 13
+	hiveOpsIds["witness_update_operation"] = 11
+	hiveOpsIds["account_witness_vote_operation"] = 12
+	hiveOpsIds["account_witness_proxy_operation"] = 13
 
-    hiveOpsIds["pow_operation"] = 14
+	hiveOpsIds["pow_operation"] = 14
 
-    hiveOpsIds["custom_operation"] = 15
+	hiveOpsIds["custom_operation"] = 15
 
-    hiveOpsIds["report_over_production_operation"] = 16
+	hiveOpsIds["report_over_production_operation"] = 16
 
-    hiveOpsIds["delete_comment_operation"] = 17
-    hiveOpsIds["custom_json_operation"] = 18
-    hiveOpsIds["comment_options_operation"] = 19
-    hiveOpsIds["set_withdraw_vesting_route_operation"] = 20
-    hiveOpsIds["limit_order_create2_operation"] = 21
-    hiveOpsIds["claim_account_operation"] = 22
-    hiveOpsIds["create_claimed_account_operation"] = 23
-    hiveOpsIds["request_account_recovery_operation"] = 24
-    hiveOpsIds["recover_account_operation"] = 25
-    hiveOpsIds["change_recovery_account_operation"] = 26
-    hiveOpsIds["escrow_transfer_operation"] = 27
-    hiveOpsIds["escrow_dispute_operation"] = 28
-    hiveOpsIds["escrow_release_operation"] = 29
-    hiveOpsIds["pow2_operation"] = 30
-    hiveOpsIds["escrow_approve_operation"] = 31
-    hiveOpsIds["transfer_to_savings_operation"] = 32
-    hiveOpsIds["transfer_from_savings_operation"] = 33
-    hiveOpsIds["cancel_transfer_from_savings_operation"] = 34
-    hiveOpsIds["custom_binary_operation"] = 35
-    hiveOpsIds["decline_voting_rights_operation"] = 36
-    hiveOpsIds["reset_account_operation"] = 37
-    hiveOpsIds["set_reset_account_operation"] = 38
-    hiveOpsIds["claim_reward_balance_operation"] = 39
-    hiveOpsIds["delegate_vesting_shares_operation"] = 40
-    hiveOpsIds["account_create_with_delegation_operation"] = 41
-    hiveOpsIds["witness_set_properties_operation"] = 42
-    hiveOpsIds["account_update2_operation"] = 43
-    hiveOpsIds["create_proposal_operation"] = 44
-    hiveOpsIds["update_proposal_votes_operation"] = 45
-    hiveOpsIds["remove_proposal_operation"] = 46
-    hiveOpsIds["update_proposal_operation"] = 47
-    hiveOpsIds["collateralized_convert_operation"] = 48
-    hiveOpsIds["recurrent_transfer_operation"] = 49
+	hiveOpsIds["delete_comment_operation"] = 17
+	hiveOpsIds["custom_json_operation"] = 18
+	hiveOpsIds["comment_options_operation"] = 19
+	hiveOpsIds["set_withdraw_vesting_route_operation"] = 20
+	hiveOpsIds["limit_order_create2_operation"] = 21
+	hiveOpsIds["claim_account_operation"] = 22
+	hiveOpsIds["create_claimed_account_operation"] = 23
+	hiveOpsIds["request_account_recovery_operation"] = 24
+	hiveOpsIds["recover_account_operation"] = 25
+	hiveOpsIds["change_recovery_account_operation"] = 26
+	hiveOpsIds["escrow_transfer_operation"] = 27
+	hiveOpsIds["escrow_dispute_operation"] = 28
+	hiveOpsIds["escrow_release_operation"] = 29
+	hiveOpsIds["pow2_operation"] = 30
+	hiveOpsIds["escrow_approve_operation"] = 31
+	hiveOpsIds["transfer_to_savings_operation"] = 32
+	hiveOpsIds["transfer_from_savings_operation"] = 33
+	hiveOpsIds["cancel_transfer_from_savings_operation"] = 34
+	hiveOpsIds["custom_binary_operation"] = 35
+	hiveOpsIds["decline_voting_rights_operation"] = 36
+	hiveOpsIds["reset_account_operation"] = 37
+	hiveOpsIds["set_reset_account_operation"] = 38
+	hiveOpsIds["claim_reward_balance_operation"] = 39
+	hiveOpsIds["delegate_vesting_shares_operation"] = 40
+	hiveOpsIds["account_create_with_delegation_operation"] = 41
+	hiveOpsIds["witness_set_properties_operation"] = 42
+	hiveOpsIds["account_update2_operation"] = 43
+	hiveOpsIds["create_proposal_operation"] = 44
+	hiveOpsIds["update_proposal_votes_operation"] = 45
+	hiveOpsIds["remove_proposal_operation"] = 46
+	hiveOpsIds["update_proposal_operation"] = 47
+	hiveOpsIds["collateralized_convert_operation"] = 48
+	hiveOpsIds["recurrent_transfer_operation"] = 49
 
-    return hiveOpsIds
+	return hiveOpsIds
 }
--- serializer_test.go
+++ serializer_test.go
@@ -1,103 +1,103 @@
 package hivego
 
 import (
-    "bytes"
-    "testing"
+	"bytes"
+	"testing"
 )
 
 func TestOpIdB(t *testing.T) {
-    got := opIdB("custom_json")
-    expected := byte(18)
+	got := opIdB("custom_json")
+	expected := byte(18)
 
-    if got != expected {
-        t.Error("Expected", expected, "got")
-    }
+	if got != expected {
+		t.Error("Expected", expected, "got")
+	}
 }
 
 func TestRefBlockNumB(t *testing.T) {
-    got := refBlockNumB(36029)
-    expected := []byte{189, 140}
+	got := refBlockNumB(36029)
+	expected := []byte{189, 140}
 
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestRefBlockPrefixB(t *testing.T) {
-    got := refBlockPrefixB(1164960351)
-    expected := []byte{95, 226, 111, 69}
+	got := refBlockPrefixB(1164960351)
+	expected := []byte{95, 226, 111, 69}
 
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestExpTimeB(t *testing.T) {
-    got, _ := expTimeB("2016-08-08T12:24:17")
-    expected := []byte{241, 121, 168, 87}
+	got, _ := expTimeB("2016-08-08T12:24:17")
+	expected := []byte{241, 121, 168, 87}
 
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestCountOpsB(t *testing.T) {
-    got := countOpsB(getTwoTestOps())
-    expected := []byte{2}
+	got := countOpsB(getTwoTestOps())
+	expected := []byte{2}
 
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 //func TestExtensionsB
 
 func TestAppendVString(t *testing.T) {
-    var buf bytes.Buffer
-    got := appendVString("xeroc", &buf)
-    expected := []byte{5, 120, 101, 114, 111, 99}
-    if !bytes.Equal(got.Bytes(), expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	var buf bytes.Buffer
+	got := appendVString("xeroc", &buf)
+	expected := []byte{5, 120, 101, 114, 111, 99}
+	if !bytes.Equal(got.Bytes(), expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestAppendVStringArray(t *testing.T) {
-    var buf bytes.Buffer
-    got := appendVStringArray([]string{"xeroc", "piston"}, &buf).Bytes()
-    expected := []byte{2, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110}
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	var buf bytes.Buffer
+	got := appendVStringArray([]string{"xeroc", "piston"}, &buf).Bytes()
+	expected := []byte{2, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110}
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestSerializeTx(t *testing.T) {
-    got, _ := serializeTx(getTestVoteTx())
-    expected := []byte{189, 140, 95, 226, 111, 69, 241, 121, 168, 87, 1, 0, 5, 120, 101, 114, 111, 99, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110, 16, 39, 0}
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	got, _ := serializeTx(getTestVoteTx())
+	expected := []byte{189, 140, 95, 226, 111, 69, 241, 121, 168, 87, 1, 0, 5, 120, 101, 114, 111, 99, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110, 16, 39, 0}
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestSerializeOps(t *testing.T) {
-    got, _ := serializeOps(getTwoTestOps())
-    expected := []byte{2, 0, 5, 120, 101, 114, 111, 99, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110, 16, 39, 18, 0, 1, 5, 120, 101, 114, 111, 99, 7, 116, 101, 115, 116, 45, 105, 100, 17, 123, 34, 116, 101, 115, 116, 107, 34, 58, 34, 116, 101, 115, 116, 118, 34, 125}
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	got, _ := serializeOps(getTwoTestOps())
+	expected := []byte{2, 0, 5, 120, 101, 114, 111, 99, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110, 16, 39, 18, 0, 1, 5, 120, 101, 114, 111, 99, 7, 116, 101, 115, 116, 45, 105, 100, 17, 123, 34, 116, 101, 115, 116, 107, 34, 58, 34, 116, 101, 115, 116, 118, 34, 125}
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestSerializeOpVoteOperation(t *testing.T) {
-    got, _ := getTestVoteOp().serializeOp()
-    expected := []byte{0, 5, 120, 101, 114, 111, 99, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110, 16, 39}
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	got, _ := getTestVoteOp().serializeOp()
+	expected := []byte{0, 5, 120, 101, 114, 111, 99, 5, 120, 101, 114, 111, 99, 6, 112, 105, 115, 116, 111, 110, 16, 39}
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
 
 func TestSerializeOpCustomJsonOperation(t *testing.T) {
-    got, _ := getTestCustomJsonOp().serializeOp()
-    expected := []byte{18, 0, 1, 5, 120, 101, 114, 111, 99, 7, 116, 101, 115, 116, 45, 105, 100, 17, 123, 34, 116, 101, 115, 116, 107, 34, 58, 34, 116, 101, 115, 116, 118, 34, 125}
-    if !bytes.Equal(got, expected) {
-        t.Error("Expected", expected, "got", got)
-    }
+	got, _ := getTestCustomJsonOp().serializeOp()
+	expected := []byte{18, 0, 1, 5, 120, 101, 114, 111, 99, 7, 116, 101, 115, 116, 45, 105, 100, 17, 123, 34, 116, 101, 115, 116, 107, 34, 58, 34, 116, 101, 115, 116, 118, 34, 125}
+	if !bytes.Equal(got, expected) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
